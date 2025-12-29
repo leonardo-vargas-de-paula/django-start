ffb37d245ccdecf70efa63e5b4c3e6d75339bafd
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2025-12-29 10:19:53 -0400

    feat: cria classe QuestionAdmin para exibir ID no admin

--- django_start/polls/admin.py
+++ django_start/polls/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Question
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id','__str__', 'pub_date')
+    
+    search_fields = ['id']
+admin.site.register(Question, QuestionAdmin)
